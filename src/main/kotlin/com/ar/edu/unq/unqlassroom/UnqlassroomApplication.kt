6fb47114da1355fc85345ffaa149bd4013bcc075
thiago.sofarelli <sofarelli.thiago10@gmail.com>
2026-08-26 20:03:55 -0300

    Modifico zona horaria

--- src/main/kotlin/com/ar/edu/unq/unqlassroom/UnqlassroomApplication.kt
+++ src/main/kotlin/com/ar/edu/unq/unqlassroom/UnqlassroomApplication.kt
@@ -2,10 +2,12 @@ package com.ar.edu.unq.unqlassroom
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class UnqlassroomApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"))
 	runApplication<UnqlassroomApplication>(*args)
 }
