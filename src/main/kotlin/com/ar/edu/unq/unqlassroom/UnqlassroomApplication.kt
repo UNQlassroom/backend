75e783779a1f27a906fdb04d9a5602959a29dd74
thiago.sofarelli <sofarelli.thiago10@gmail.com>
2026-08-25 23:12:53 -0300

    Setup de spring boot, base de datos, y docker

---
+++ src/main/kotlin/com/ar/edu/unq/unqlassroom/UnqlassroomApplication.kt
@@ -0,0 +1,11 @@
+package com.ar.edu.unq.unqlassroom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnqlassroomApplication
+
+fun main(args: Array<String>) {
+	runApplication<UnqlassroomApplication>(*args)
+}
